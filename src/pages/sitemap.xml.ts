5ce0a63acdd1edd59009cefdeea5e94c27a515a0
lollo110 <lorenzopaolocilurzo@gmail.com>
2026-05-21 20:43:03 +0200

    Improve sitemap formatting

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -15,18 +15,25 @@ const staticPages = [
 ];
 
 export const GET: APIRoute = () => {
+  const lastmod = new Date().toISOString().split("T")[0];
   const pages = [
     ...staticPages,
     ...tools.filter((tool) => tool.status === "available").map((tool) => `outils/${tool.slug}`),
     ...articles.map((article) => `blog/${article.slug}`)
   ];
 
+  const toAbsoluteUrl = (page: string) => {
+    const path = page ? `/${page}/` : "/";
+    return new URL(path, site.url).toString();
+  };
+
   const body = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${pages
   .map(
     (page) => `  <url>
-    <loc>${new URL(page, site.url).toString()}</loc>
+    <loc>${toAbsoluteUrl(page)}</loc>
+    <lastmod>${lastmod}</lastmod>
   </url>`
   )
   .join("\n")}
